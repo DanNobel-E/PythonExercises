fc3fb41fcc615aa7ac9645497b809899c48956af
Simone Santagati <simone.santagati@hotmail.it>
2022-03-31 14:55:30 +0200

    fixed circle class

--- Circle.py
+++ Circle.py
@@ -1,15 +1,38 @@
-import CircleUtils
+import circle_utils
 import math
 import matplotlib.pyplot as plt
 
 class Circle:
     
+    @property 
+    def diameter(self):
+        return self.radius*2
+    
+    @property 
+    def area(self):
+        return self.radius**2 * math.pi
+    
+    @property 
+    def circumference(self):
+        return 2*self.radius * math.pi
+    
     def __init__(self, center, radius=0, diameter=0):
         self.center= center
         self.radius= abs(radius) if diameter ==0 else abs(diameter*0.5)
-        self.circumference = 2*self.radius * math.pi
-        self.area = self.radius**2 * math.pi
         
+    def __eq__(self, other):
+        return self.radius==other.radius
+    
+    def __lt__(self, other):
+        return self.radius<other.radius
+    
+    def __gt__(self, other):
+        return self.radius>other.radius
+    
+    def __add__ (self, other):
+        center= (self.center[0]+other.center[0],self.center[1],other.center[1])
+        return Circle(center,self.radius+other.radius)
+    
     def draw(self,color, x_lim= (-1,1), y_lim= (-1,1)):
         circle=plt.Circle(self.center,self.radius,color= color)
         fig, ax = plt.subplots() 
@@ -40,7 +63,7 @@ if __name__ == '__main__':
     circle0= Circle((-7,0),3.14)
     circle1= Circle((2,3),5)
     circle2= Circle((1,2),1)
-    c= CircleUtils.CircleUtils(circle0,circle1,circle2)
+    c= circle_utils.CircleUtils(circle0,circle1,circle2)
     #c.sort("area")
     c.print()
     c.draw(['red','blue','white'], (-10,10), (-10,10))
